b068e9b639edfac360779ce986f2c42e9ccfd7bf
phelo Mabindisa <nathimabindisa@gmail.com>
2024-07-27 02:30:51 +0200

    Initialization message, Adding alx_book_store.sql file

---
+++ alx_book_store.sql
@@ -0,0 +1,32 @@
+CREATE TABLE Books(
+    book_id(PRIMARY KEY),
+    title VARCHAR(130),
+    author_id(),
+    price DOUBLE,
+    publication(DATE)
+)
+
+CREATE TABLE Author(
+    author_id(PRIMARY KEY),
+    author_name VARCHAR(215)
+)
+
+CREATE TABLE Customers(
+    customer_id(PRIMARY KEY),
+    customer_name VARCHAR(215),
+    email VARCHAR(215),
+    address TEXT
+)
+
+CREATE TABLE Orders(
+    order_id(PRIMARY KEY),
+    customer_id(),
+    order_date DATE
+)
+
+CREATE TABLE Orders_Details(
+    order_details_id(PRIMARY KEY),
+    order_id(),
+    book_id(),
+    quantity DOUBLE
+)
