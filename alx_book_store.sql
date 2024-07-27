03c80b14f7a38d513ce04f1d568c670eed8109fa
phelo Mabindisa <nathimabindisa@gmail.com>
2024-07-27 02:45:15 +0200

    Updating alx_book_store.sql file

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,32 +1,34 @@
+CREATE DATABASE IF NOT EXISTS alx_book_store;
+
 CREATE TABLE Books(
     book_id(PRIMARY KEY),
     title VARCHAR(130),
-    author_id(),
+    author_id(author_id),
     price DOUBLE,
     publication(DATE)
-)
+);
 
 CREATE TABLE Author(
     author_id(PRIMARY KEY),
     author_name VARCHAR(215)
-)
+);
 
 CREATE TABLE Customers(
     customer_id(PRIMARY KEY),
     customer_name VARCHAR(215),
     email VARCHAR(215),
     address TEXT
-)
+);
 
 CREATE TABLE Orders(
     order_id(PRIMARY KEY),
-    customer_id(),
+    customer_id(customer_id),
     order_date DATE
-)
+);
 
 CREATE TABLE Orders_Details(
     order_details_id(PRIMARY KEY),
-    order_id(),
+    order_id(order_id),
     book_id(),
     quantity DOUBLE
-)
+);
