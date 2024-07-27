9a512c8cddbe202748ec31633a1d818d205e6ee6
phelo Mabindisa <nathimabindisa@gmail.com>
2024-07-27 02:58:44 +0200

    Updating alx_book_store.sql file

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -27,9 +27,11 @@ CREATE TABLE Orders(
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
-CREATE TABLE Orders_Details(
+CREATE TABLE Order_Details(
     order_details_id(PRIMARY KEY),
     order_id INT,
-    book_id(),
-    quantity DOUBLE
+    book_id INT,
+    quantity DOUBLE,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
